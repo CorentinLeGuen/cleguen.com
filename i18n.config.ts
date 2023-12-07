bcc61d07b59078efc51097787a997258abfa2a77
CorentinLeGuen <leguen.corentin@protonmail.com>
2023-12-07 10:55:06 -0500

    Add first translation

--- i18n.config.ts
+++ i18n.config.ts
@@ -3,10 +3,18 @@ export default defineI18nConfig(() => ({
     locale: 'en',
     messages: {
         en: {
-            welcome: 'Welcome'
+            welcome: 'Welcome',
+            under_construction: 'This website is under construction.',
+            contact_intro: 'You can still',
+            contact: 'contact me',
+            under_construction_translate: 'Some pages are not yet translated.'
         },
         fr: {
-            welcome: 'Bienvenue'
+            welcome: 'Bienvenue',
+            under_construction: 'Ce site est en cours de construction.',
+            contact_intro: 'Vous pouvez toujours ',
+            contact: 'me contacter',
+            under_construction_translate: 'La plupart des pages ne sont pas encore traduites en français.'
         }
     }
 }))
