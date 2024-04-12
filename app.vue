9378edc74693e01d89b868a7c126cd8e67f0da00
CorentinLeGuen <leguen.corentin@protonmail.com>
2024-04-12 07:57:08 -0500

    Add cookie banner

--- app.vue
+++ app.vue
@@ -35,6 +35,24 @@
                 <NuxtPage />
             </div>
 
+            <div id="bottom-banner" tabindex="-1" class="fixed bottom-0 start-0 z-25 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-50">
+                <div class="flex items-center mx-auto">
+                    <p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
+                        <span>
+                            🍪 {{ $t('cookie_policy') }}
+                        </span>
+                    </p>
+                </div>
+                <div class="flex items-center">
+                    <button data-dismiss-target="#bottom-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close banner</span>
+                    </button>
+                </div>
+            </div>
+
             <footer class="bg-white rounded-lg shadow m-3 mt-9">
                 <div class="w-full mx-auto max-w-screen-md p-3 flex items-center justify-between">
                     <span class="text-sm text-gray-500 sm:text-center">Cleguen © 2023 - {{ $t('all_right_reserved') }}</span>
