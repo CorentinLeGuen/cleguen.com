5f52df02fc025f2d3d99e3b560a0da9542611330
CorentinLeGuen <leguen.corentin@protonmail.com>
2024-04-16 10:18:45 -0500

    Changed cookie alert

--- app.vue
+++ app.vue
@@ -35,28 +35,23 @@
                 <NuxtPage />
             </div>
 
-            <div id="bottom-banner" tabindex="-1" class="fixed bottom-0 start-0 z-25 flex justify-between w-full p-4 border-t border-gray-200 bg-gray-50">
-                <div class="flex items-center mx-auto">
-                    🍪
-                    <p class="flex items-center mx-2 text-center text-sm font-normal text-gray-500">
-                        <span>
-                            {{ $t('cookie_policy') }}
-                        </span>
-                    </p>
+            <div id="alert-c" class="fixed bottom-0 right-0 flex items-center p-4 border rounded-lg bg-gray-50 shadow m-2 mb-8" role="alert">
+                🍪
+                <span class="sr-only">Info</span>
+                <div class="ms-3 text-sm text-center font-medium text-gray-800">
+                    {{  $t('cookie_policy') }}
                 </div>
-                <div class="flex items-center">
-                    <button data-dismiss-target="#bottom-banner" type="button" class="flex-shrink-0 inline-flex justify-center w-7 h-7 items-center text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 dark:hover:bg-gray-600 dark:hover:text-white">
-                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
-                        </svg>
-                        <span class="sr-only">Close banner</span>
-                    </button>
-                </div>
+                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-gray-50 text-gray-500 rounded-lg focus:ring-2 focus:ring-gray-400 p-1.5 hover:bg-gray-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white" data-dismiss-target="#alert-c" aria-label="Close">
+                    <span class="sr-only">Dismiss</span>
+                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                    </svg>
+                </button>
             </div>
 
             <footer class="bg-white rounded-lg shadow m-3 mt-9">
                 <div class="w-full mx-auto max-w-screen-md p-3 flex items-center justify-between">
-                    <span class="text-sm text-gray-500 sm:text-center">Cleguen © 2023 - {{ $t('all_right_reserved') }}</span>
+                    <span class="text-sm text-gray-500 sm:text-center">Corentin Le Guen © 2024 - {{ $t('all_right_reserved') }}</span>
                     <NuxtLink class="me-3 md:me-5 capitalize" to="/about">{{ $t('about.title') }}</NuxtLink>
                 </div>
             </footer>
