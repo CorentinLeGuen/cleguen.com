f99ab969877af4824345270df01443bd910e965a
CorentinLeGuen <leguen.corentin@protonmail.com>
2024-04-22 15:10:50 -0500

    Change grid system on posts index page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -8,13 +8,11 @@
             <h1 class="text-center text-4xl uppercase font-extrabold tracking-tight leading-none text-transparent bg-clip-text bg-gradient-to-br from-blue-400 to-forest-500">{{ $t('posts.title') }}</h1>
             <hr class="w-1/3 h-1 mx-auto my-16 bg-gradient-to-br from-blue-50 to-forest-100 border-0 rounded"/>
 
-            <div class="grid justify-items-center gap-x-8 gap-y-4 xl:grid-cols-3 md:grid-cols-3 sm:grid-cols-2">
+            <div class="grid justify-items-center gap-x-8 gap-y-4 md:grid-cols-2 xl:grid-cols-3">
                 <NuxtLink to="/posts/lotto649">
-                    <figure
-                        class="transition-all mx-4 my-4 duration-300 cursor-pointer filter grayscale hover:grayscale-0 border rounded-lg shadow-md hover:shadow-lg">
+                    <figure class="transition-all mx-4 my-4 duration-300 cursor-pointer filter grayscale hover:grayscale-0 border rounded-lg shadow-md hover:shadow-lg">
                         <img class="rounded-lg" src="/img/posts/lotto.jpg" alt="image description">
-                        <figcaption
-                            class="text-center absolute text-2xl font-semibold text-white bottom-2 left-2">
+                        <figcaption class="absolute text-2xl md:text-3xl xl:text-4xl font-semibold font-mono text-white bottom-2 left-2">
                             <p>Lotto 6.49</p>
                         </figcaption>
                     </figure>
@@ -23,8 +21,7 @@
                     <figure
                         class="transition-all mx-4 my-4 duration-300 cursor-pointer filter grayscale hover:grayscale-0 border rounded-lg shadow-md hover:shadow-lg">
                         <img class="rounded-lg" src="/img/posts/git.jpg" alt="image description">
-                        <figcaption
-                            class="text-center absolute text-2xl font-semibold text-black bottom-2 left-2">
+                        <figcaption class="absolute text-2xl md:text-3xl xl:text-4xl font-semibold font-mono text-black bottom-2 left-2">
                             <p>Git Cheat Sheet</p>
                         </figcaption>
                     </figure>
@@ -33,8 +30,7 @@
                     <figure
                         class="transition-all mx-4 my-4 duration-300 cursor-pointer filter grayscale hover:grayscale-0 border rounded-lg shadow-md hover:shadow-lg">
                         <img class="rounded-lg" src="/img/posts/3dprint.png" alt="image description">
-                        <figcaption
-                            class="text-center absolute text-2xl font-semibold text-white bottom-2 left-2">
+                        <figcaption class="absolute text-2xl md:text-3xl xl:text-4xl font-semibold font-mono text-white bottom-2 left-2">
                             <p>3D Print</p>
                         </figcaption>
                     </figure>
