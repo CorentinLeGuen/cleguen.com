8eb7cd47d0dacd0ca91533a958048e7a5b61cf88
CorentinLeGuen <leguen.corentin@protonmail.com>
2024-04-22 13:22:25 -0500

    Add Posts hover underline

--- app.vue
+++ app.vue
@@ -18,9 +18,9 @@
                     </button>
                     <div class="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
                         <ul
-                            class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent ">
+                            class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
                             <li>
-                                <NuxtLink class="block py-2 px-3 md:p-0 text-gray-900 rounded md:hover:bg-transparent md:border-0 hover:text-forest-500"
+                                <NuxtLink class="block py-2 px-3 md:p-0 text-gray-500 rounded md:hover:bg-transparent md:border-0 hover:underline hover:decoration-2 hover:underline-offset-2 hover:decoration-gray-400"
                                     to="/posts">{{ $t('posts.title') }}</NuxtLink>
                             </li>
                         </ul>
