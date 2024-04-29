1fe0ae10d56c081e736def4d483ffa055dcdc3e2
CorentinLeGuen <leguen.corentin@protonmail.com>
2024-04-29 11:46:16 -0500

    Update timeout page and empty results

--- pages/posts/lotto649.vue
+++ pages/posts/lotto649.vue
@@ -267,132 +267,110 @@
 
                 <!-- Examples of results -->
                 <div class="w-5/6 md:w-2/3 xl:w-1/3 mx-auto">
-                    <div id="accordion-collapse" data-accordion="collapse">
-                        <h2 id="accordion-collapse-heading-1">
-                            <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 focus:ring-4 focus:ring-gray-200 hover:bg-gray-100 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
-                                <span class="mx-auto">
-                                    {{ $t('posts.lotto649.api.example_stats') }}
-                                </span>
-                                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
+                    <!-- Stats -->
+                    <div class="p-5">
+                        <h2 class="text-center text-semibold text-xl underline underlin-offset-2 pb-2">{{ $t('posts.lotto649.api.example_stats') }}</h2>
+                        <div v-if="pending_stats">
+                            <div role="status" class="text-center">
+                                <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin fill-green-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
                                 </svg>
-                            </button>
-                        </h2>
-                        <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-                            <div class="p-5">
-                                <div v-if="pending_stats">
-                                    <div role="status" class="text-center">
-                                        <svg aria-hidden="true" class="inline w-8 h-8 text-gray-200 animate-spin fill-green-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-                                            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-                                        </svg>
-                                        <span class="sr-only">Loading...</span>
-                                    </div>
-                                </div>
-                                <div v-else-if="error_stats">
-                                    <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 justify-center" role="alert">
-                                        <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                                            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-                                        </svg>
-                                        <span class="sr-only">Info</span>
-                                        <div class="text-center">
-                                            <span class="font-medium">Sorry !</span> The stats API is presently not available.
-                                        </div>
-                                    </div>
-                                </div>
-                                <div v-else>
-                                    <div class="text-center my-4">
-                                        Stats example with a total of <span class="text-red-400 font-semibold">{{ data_stats.total }}</span> draws, from {{ data_stats.date_start }} to {{ data_stats.date_end }}
-                                    </div>
-                                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
-                                        <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-                                            <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-                                                <tr class="bg-white border-b hover:bg-gray-50">
-                                                    <th scope="col" class="px-6 py-2">#</th>
-                                                    <th scope="col" class="px-6 py-2 text-center">Total</th>
-                                                    <th scope="col" class="px-6 py-2 text-center">Complementary</th>
-                                                </tr>
-                                            </thead>
-                                            <tbody>
-                                                <tr class="bg-white border-b hover:bg-gray-50" v-for="([key, value]) in sorted_data_stats" :key="key">
-                                                    <th scope="row" class="px-6 py-2">{{ key.replace('number_', 'N. ') }}</th>
-                                                    <td class="px-6 py-2 text-center">{{ value.total_occurrence }}</td>
-                                                    <td class="px-6 py-2 text-center">{{ value.complementary }}</td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+                        <div v-else-if="error_stats">
+                            <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 justify-center" role="alert">
+                                <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                                </svg>
+                                <span class="sr-only">Info</span>
+                                <div class="text-center">
+                                    <span class="font-medium">Sorry !</span> The stats API is presently not available.
                                 </div>
                             </div>
                         </div>
-                        <h2 id="accordion-collapse-heading-2">
-                            <button type="button" class="flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-2" aria-expanded="false" aria-controls="accordion-collapse-body-2">
-                                <span class="mx-auto">
-                                    {{ $t('posts.lotto649.api.example_pred') }}
-                                </span>
-                                <svg data-accordion-icon class="w-3 h-3 rotate-180 shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5"/>
-                                </svg>
-                            </button>
-                        </h2>
-                        <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
-                            <div class="p-5">
-                                <div class="text-center my-4 text-gray-500">
-                                    Prediction for <span class="text-red-400 font-semibold" >2024-04-20</span>
-                                </div>
-                                <table class="w-full text-sm text-gray-500 text-center">
-                                    <thead>
-                                        <tr class="border py-2 px-6 bg-gray-50">
-                                            <th>Number</th>
-                                            <th>Actual</th>
-                                            <th>Predicted</th>
+                        <div v-else>
+                            <div class="text-center my-4">
+                                Stats example with a total of <span class="text-red-400 font-semibold">{{ data_stats.total }}</span> draws, from {{ data_stats.date_start }} to {{ data_stats.date_end }}
+                            </div>
+                            <div class="relative overflow-x-auto shadow-md sm:rounded-lg mt-5">
+                                <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+                                    <thead class="text-xs text-gray-700 uppercase bg-gray-50">
+                                        <tr class="bg-white border-b hover:bg-gray-50">
+                                            <th scope="col" class="px-6 py-2">#</th>
+                                            <th scope="col" class="px-6 py-2 text-center">Total</th>
+                                            <th scope="col" class="px-6 py-2 text-center">Complementary</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50">
-                                            <td>1</td>
-                                            <td>9</td>
-                                            <td class="text-forest-300 font-semibold">9</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50">
-                                            <td>2</td>
-                                            <td>14</td>
-                                            <td class="text-forest-300 font-semibold">14</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50">
-                                            <td>3</td>
-                                            <td>23</td>
-                                            <td class="text-red-300 font-semibold">22</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50">
-                                            <td>4</td>
-                                            <td>26</td>
-                                            <td class="text-red-300 font-semibold">28</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50">
-                                            <td>5</td>
-                                            <td>30</td>
-                                            <td class="text-red-300 font-semibold">35</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50">
-                                            <td>6</td>
-                                            <td>43</td>
-                                            <td class="text-forest-300 font-semibold">43</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center hover:bg-gray-50 border-b">
-                                            <td>c</td>
-                                            <td>35</td>
-                                            <td class="text-red-300 font-semibold">34</td>
-                                        </tr>
-                                        <tr class="px-6 py-2 text-center font-semibold hover:bg-gray-50">
-                                            <td>Accurracy</td>
-                                            <td>42.86 %</td>
-                                            <td>$ 10.00 gain</td>
+                                        <tr class="bg-white border-b hover:bg-gray-50" v-for="([key, value]) in sorted_data_stats" :key="key">
+                                            <th scope="row" class="px-6 py-2">{{ key.replace('number_', 'N. ') }}</th>
+                                            <td class="px-6 py-2 text-center">{{ value.total_occurrence }}</td>
+                                            <td class="px-6 py-2 text-center">{{ value.complementary }}</td>
                                         </tr>
                                     </tbody>
                                 </table>
                             </div>
                         </div>
+                    </div>
+                    <!-- Predictions -->
+                    <div class="p-5">
+                        <h2 class="text-center text-semibold text-xl underline underlin-offset-2 pb-2">{{ $t('posts.lotto649.api.example_pred') }}</h2>
+                        <div class="text-center my-4 text-gray-500">
+                            Prediction for <span class="text-red-400 font-semibold" >2024-04-20</span>
+                        </div>
+                        <table class="w-full text-sm text-gray-500 text-center">
+                            <thead>
+                                <tr class="border py-2 px-6 bg-gray-50">
+                                    <th>Number</th>
+                                    <th>Actual</th>
+                                    <th>Predicted</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50">
+                                    <td>1</td>
+                                    <td>9</td>
+                                    <td class="text-forest-300 font-semibold">9</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50">
+                                    <td>2</td>
+                                    <td>14</td>
+                                    <td class="text-forest-300 font-semibold">14</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50">
+                                    <td>3</td>
+                                    <td>23</td>
+                                    <td class="text-red-300 font-semibold">22</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50">
+                                    <td>4</td>
+                                    <td>26</td>
+                                    <td class="text-red-300 font-semibold">28</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50">
+                                    <td>5</td>
+                                    <td>30</td>
+                                    <td class="text-red-300 font-semibold">35</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50">
+                                    <td>6</td>
+                                    <td>43</td>
+                                    <td class="text-forest-300 font-semibold">43</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center hover:bg-gray-50 border-b">
+                                    <td>c</td>
+                                    <td>35</td>
+                                    <td class="text-red-300 font-semibold">34</td>
+                                </tr>
+                                <tr class="px-6 py-2 text-center font-semibold hover:bg-gray-50">
+                                    <td>Accurracy</td>
+                                    <td>42.86 %</td>
+                                    <td>$ 10.00 gain</td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
 
@@ -482,8 +460,8 @@
 </template>
 
 <script setup lang="ts">
-const { pending: pending_last, error: error_last, data: data_last } = await useFetch('/draw/last', { timeout: 1200, server: false });
-const { pending: pending_stats, error: error_stats, data: data_stats } = await useFetch('/draw/stats', { timeout: 1200, server: false });
+const { pending: pending_last, error: error_last, data: data_last } = useFetch('/draw/last', { server: false });
+const { pending: pending_stats, error: error_stats, data: data_stats } = useFetch('/draw/stats', { server: false });
 
 const sorted_data_stats = computed(() => {
     const items = Object.entries(data_stats.value.draws);
