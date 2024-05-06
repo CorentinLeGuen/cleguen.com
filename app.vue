31a5e1bdc88532b7c21fb0ac0a8afbdc762ee32b
CorentinLeGuen <leguen.corentin@protonmail.com>
2024-05-06 14:39:00 -0500

    Update cookie section padding

--- app.vue
+++ app.vue
@@ -38,10 +38,10 @@
             <div id="alert-c" class="fixed bottom-0 right-0 flex items-center p-4 border rounded-lg bg-gray-50 shadow m-2 mb-8" role="alert">
                 🍪
                 <span class="sr-only">Info</span>
-                <div class="ms-3 text-sm text-center font-medium text-gray-800">
+                <div class="ms-3 px-4 text-sm text-center font-medium text-gray-800">
                     {{  $t('cookie_policy') }}
                 </div>
-                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-gray-50 text-gray-500 rounded-lg focus:ring-2 focus:ring-gray-400 p-1.5 hover:bg-gray-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white" data-dismiss-target="#alert-c" aria-label="Close">
+                <button type="button" class="ms-auto mx-1.5 -my-1.5 bg-gray-50 text-gray-500 rounded-lg focus:ring-2 focus:ring-gray-400 p-1.5 hover:bg-gray-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-gray-300 dark:hover:bg-gray-700 dark:hover:text-white" data-dismiss-target="#alert-c" aria-label="Close">
                     <span class="sr-only">Dismiss</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
