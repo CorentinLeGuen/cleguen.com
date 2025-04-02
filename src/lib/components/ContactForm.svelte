b4e7a753035a6ff0b673688f25972d3c05bdec39
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-04-02 15:06:27 -0400

    Rewrite 2xl screen text format

---
+++ src/lib/components/ContactForm.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { Input, Label, Helper } from 'flowbite-svelte';
+</script>
+
+<Label class="block space-y-2">
+	<span>Your email</span>
+	<Input label="Email" id="email" name="email" required placeholder="name@flowbite.com" />
+	<Helper class="text-sm">
+		We’ll never share your details. Read our <a
+			href="/"
+			class="text-primary-600 dark:text-primary-500 font-medium hover:underline"
+		>
+			Privacy Policy
+		</a>
+		.
+	</Helper>
+</Label>
