3f0e981fc6d0d25dfeb54a4e60098f84fc34c94d
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-04-02 15:42:51 -0400

    Update footer rights

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -6,7 +6,7 @@
 <Footer class="border-t-1 border-gray-100 dark:border-gray-800">
 	<div class="w-full mx-auto max-w-screen-xl p-4 flex items-center justify-between">
         <span class="self-center whitespace-nowrap text-sm text-gray-700 dark:text-gray-400">
-            &copy; 2025 - <a href="/" class="font-semibold">Corentin Le Guen</a>
+            &copy; 2025 <a href="/" class="font-semibold">Corentin Le Guen</a> - {$t('translations.legal.rights')} 
         </span>
 		<FooterLinkGroup ulClass="flex flex-wrap items-center text-sm text-gray-500 dark:text-gray-400">
 			<FooterLink href="/about">{$t('translations.about.title')}</FooterLink>
