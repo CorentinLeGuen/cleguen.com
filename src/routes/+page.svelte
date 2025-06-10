52cefc0287a189728c5060674a29451940e94f0d
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-06-10 10:34:22 -0400

    :newspaper: Add small tip about free courses from University of Columbia

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,3 +17,17 @@
 		</Button>
 	</a>
 </div>
+
+<hr class="w-48 h-1 mx-auto my-4 bg-gray-100 border-0 rounded-sm md:my-10 dark:bg-gray-700">
+
+<div class="paragraph text-center shadow-xl rounded-xl p-8">
+	<h2 class="text-xl font-extrabold text-gray-600 mb-6">{$t('translations.articles.tips.columbia_course.title')}</h2>
+	<p class="text-gray-500 my-4">{$t('translations.articles.tips.columbia_course.desc')}</p>
+	<p class="text-gray-500">{@html $t('translations.articles.tips.columbia_course.info')}</p>
+	<a href="https://www.instagram.com/p/DKAqk_oz6eG/" target="_blank">
+		<img src="/images/insta_columbia.png" class="rounded-4xl shadow my-6 w-full sm:w-3/4 mx-auto" alt="instagram post" />
+	</a>
+	<a href="https://plus.columbia.edu/content/learning-ai-through-visualization" class="border border-gray-400 px-12 py-2 rounded hover:shadow" target="_blank">
+		{$t('translations.articles.tips.columbia_course.link')}
+	</a>
+</div>
