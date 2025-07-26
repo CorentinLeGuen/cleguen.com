5b8976fb95e2f4e4b61ac0fa442c14cea9c2d4f8
CorentinLeGuen <leguen.corentin@protonmail.com>
2025-07-26 17:56:01 -0400

    Add AWS badge

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,58 +6,44 @@
 	import { AwardOutline, ChartOutline } from "flowbite-svelte-icons";
 	import { Python, Github, Linkedin, Mail, } from "svelte-supertiny";
     import ContactForm from '$lib/components/ContactForm.svelte';
-
 </script>
 
-<h1 class="mb-4 text-center text-3xl font-bold text-gray-900 dark:text-gray-100">{$t('translations.about.title')}</h1>
+<div class="max-w-full sm:max-w-3/4 md:max-w-2/3 xl:max-w-1/2 2xl:max-w-1/3 mx-auto">
+	<h1 class="mb-4 text-center text-3xl font-bold text-gray-900 dark:text-gray-100">{$t('translations.about.title')}</h1>
 
-<p class="mb-4 text-center text-2xl">{$t('translations.about.short_description')}</p>
+	<p class="mb-8 text-center text-2xl">{$t('translations.about.short_description')}</p>
 
-<Section name="feature">
-	<FeatureDefault class="grid grid-cols-3 text-center my-2 sm:my-4 md:my-12">
-		<FeatureItem class="mx-auto">
-		  <svelte:fragment slot="icon"><AwardOutline class="text-primary-600 dark:text-primary-300" /></svelte:fragment>
-		  <svelte:fragment slot="h3">{$t('translations.about.features.exp')}</svelte:fragment>
-		  <svelte:fragment slot="paragraph">{$t('translations.about.features.exp_text')}</svelte:fragment>
-		</FeatureItem>
-		<FeatureItem class="mx-auto">
-			<svelte:fragment slot="icon"><Python class="text-primary-600 dark:text-primary-300" /></svelte:fragment>
-			<svelte:fragment slot="h3">Python</svelte:fragment>
-			<svelte:fragment slot="paragraph">{$t('translations.about.features.python')}</svelte:fragment>
-		</FeatureItem>
-		<FeatureItem class="mx-auto">
-			<svelte:fragment slot="icon"><ChartOutline class="text-primary-600 dark:text-primary-300" /></svelte:fragment>
-			<svelte:fragment slot="h3">{$t('translations.about.features.domain')}</svelte:fragment>
-			<svelte:fragment slot="paragraph">{$t('translations.about.features.domain_text')}</svelte:fragment>
-		</FeatureItem>
-	</FeatureDefault>
-</Section>
+	<div class="text-justify grow">
+		<p class="my-2">{@html $t('translations.about.description.1')}</p>
+		<p class="my-2">{$t('translations.about.description.2')}</p>
+		<p class="my-2">{$t('translations.about.description.3')}</p>
+		<p class="my-2">{$t('translations.about.description.4')}</p>
+		<p class="my-2">{$t('translations.about.description.5')}</p>
+	</div>
+	
+	<div class="justify-center">
+		<a href="https://www.credly.com/badges/1640cacc-f529-4c92-bcea-50b422c32a80/public_url">
+			<img class="max-w-1/2 mx-auto drop-shadow-lg" src="images/aws-certified-developer-associate.png" alt="AWS Certified Developer - Associate Badge"/>
+		</a>
+	</div>
 
+	<div class="flex justify-center my-4 sm:my-8 md:my-12">
+		<Button outline class="x-auto w-md" href="/Resume_CorentinLeGuen.pdf" target="_blank">
+			<FileLinesSolid class="me-2 h-5 w-5" />
+			{$t('translations.about.resume_download')}
+		</Button>
+	</div>
 
-<div class="paragraph text-justify">
-	<p class="my-2">{@html $t('translations.about.description.1')}</p>
-	<p class="my-2">{$t('translations.about.description.2')}</p>
-	<p class="my-2">{$t('translations.about.description.3')}</p>
-	<p class="my-2">{$t('translations.about.description.4')}</p>
-	<p class="my-2">{$t('translations.about.description.5')}</p>
-</div>
+	<Hr classHr="my-8 w-64 text-gray-900 dark:text-gray-100">{$t('translations.contact.title')}</Hr>
 
-<div class="flex justify-center my-4 sm:my-8 md:my-12">
-	<Button outline class="x-auto w-md" href="/Resume_CorentinLeGuen.pdf" target="_blank">
-		<FileLinesSolid class="me-2 h-5 w-5" />
-		{$t('translations.about.resume_download')}
-	</Button>
-</div>
-
-<Hr classHr="my-8 w-64 text-gray-900 dark:text-gray-100">{$t('translations.contact.title')}</Hr>
-
-<div class="flex justify-center gap-4">
-	<a href="https://github.com/CorentinLeGuen" target="_blank">
-		<Github class="size-12"/>
-	</a>
-	<a href="https://www.linkedin.com/in/corentinleguen/" target="_blank">
-		<Linkedin class="size-12" />
-	</a>
+	<div class="flex justify-center gap-4">
+		<a href="https://github.com/CorentinLeGuen" target="_blank">
+			<Github class="size-12"/>
+		</a>
+		<a href="https://www.linkedin.com/in/corentinleguen/" target="_blank">
+			<Linkedin class="size-12" />
+		</a>
+	</div>
 </div>
 
 <ContactForm />
